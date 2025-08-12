75540bf0d988e9384fdb570766382abdc5152fad
Mike Neilson <m.allan.neilson@gmail.com>
2025-08-12 04:33:29 -0700

    Remove unused code.

--- src/api/v1/dds_recv.rs
+++ src/api/v1/dds_recv.rs
@@ -2,20 +2,8 @@ use std::fmt::Debug;
 
 use garde::Validate;
 use kube::CustomResource;
-use schemars::visit::{visit_schema_object, Visitor};
 use schemars::{schema::SchemaObject, JsonSchema};
 use serde::{Deserialize, Serialize};
-
-pub struct MyVisitor;
-
-impl Visitor for MyVisitor {
-    fn visit_schema_object(&mut self, schema: &mut SchemaObject) {
-        schema
-            .extensions
-            .insert("test".to_string(), serde_json::json!("a test"));
-        visit_schema_object(self, schema);
-    }
-}
 
 // Our custom resource
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema, Validate)]
@@ -26,7 +14,6 @@ impl Visitor for MyVisitor {
     namespaced
 )]
 #[serde(rename_all = "camelCase")]
-#[schemars(schema_with = "add_one_of")]
 pub struct DdsConnectionSpec {
     #[garde(ascii, length(min = 1))]
     pub hostname: String,
@@ -41,15 +28,6 @@ pub struct DdsConnectionSpec {
     #[garde(skip)]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub tls_mode: Option<TlsMode>,
-}
-
-#[allow(unused)]
-fn add_one_of(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
-    let mut schema = String::json_schema(gen);
-
-    // doesn't seem to provide the control desired.
-
-    schema
 }
 
 fn port_default() -> i32 {
