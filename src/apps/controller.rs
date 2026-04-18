7f05a14e54b65ff9d0db6b73a3cc8a9a6c170724
M. Allan Neilson <m.allan.neilson@gmail.com>
2026-04-18 16:44:34 -0700

    initial structure of the apps controller and schema.

---
+++ src/apps/controller.rs
@@ -0,0 +1,105 @@
+use std::{collections::BTreeMap, sync::Arc, time::Duration};
+
+use crate::{
+    api::{
+        constants::TSDB_GROUP,
+        v1::tsdb::{app::{OpenDcsApp, OpenDcsAppStatus}, database::{MigrationState, OpenDcsDatabase}},
+    },
+    telemetry::{
+        state::{Context, State},
+        telemetry,
+    },
+};
+use anyhow::anyhow;
+use chrono::Utc;
+use futures::StreamExt;
+use k8s_openapi::{
+    ByteString,
+    api::{batch::v1::Job, core::v1::Secret},
+};
+use kube::{
+    Api, Client, Error, Resource, ResourceExt,
+    api::{ObjectMeta, Patch, PatchParams},
+    runtime::{Controller, controller::Action, watcher},
+};
+use serde_json::json;
+use tracing::{Span, field, info, instrument, warn};
+
+pub async fn run(state: State<OpenDcsApp>, client: Client) {
+    let apps: Api<OpenDcsApp> = Api::all(client.clone());
+    let jobs: Api<Job> = Api::all(client.clone());
+    let secrets: Api<Secret> = Api::all(client.clone());
+    println!("Starting controller");
+    Controller::new(apps.clone(), watcher::Config::default())
+        .owns(jobs, watcher::Config::default())
+        .owns(secrets.clone(), watcher::Config::default())
+        //     .owns(services.clone(), watcher::Config::default())
+        //     .owns(cm, watcher::Config::default())
+        //     .watches(secrets.clone(), user_watch_config, user_mapper)
+        //     .watches(
+        //         dds_connections.clone(),
+        //         watcher::Config::default(),
+        //         dds_mapper,
+        //     )
+        .shutdown_on_signal()
+        .run(reconcile, error_policy, state.to_context(client).await)
+        .filter_map(|x| async move { std::result::Result::ok(x) })
+        .for_each(|_| futures::future::ready(()))
+        .await;
+}
+
+#[instrument(skip(object, ctx), fields(trace_id))]
+async fn reconcile(
+    object: Arc<OpenDcsApp>,
+    ctx: Arc<Context<OpenDcsApp>>,
+) -> Result<Action, Error> {
+    let trace_id = telemetry::get_trace_id();
+    if trace_id != opentelemetry::trace::TraceId::INVALID {
+        Span::current().record("trace_id", field::display(&trace_id));
+    }
+    let _timer = ctx.metrics.reconcile.count_and_measure(&trace_id);
+    ctx.diagnostics.write().await.last_event = Utc::now();
+    let client = &ctx.client;
+    let name = object.metadata.name.clone().unwrap();
+    let ns = object
+        .metadata
+        .namespace
+        .clone()
+        .unwrap_or("default".to_string());
+    info!("Processing \"{}\" in {}", object.name_any(), ns);
+    let apps: Api<OpenDcsApp> = Api::namespaced(client.clone(), &ns);
+    let secrets: Api<Secret> = Api::namespaced(client.clone(), &ns);
+    let databases: Api<OpenDcsDatabase> = Api::namespaced(client.clone(), &ns);
+    let patch_name = "app-controller";
+
+    let db_name = &object.spec.database;
+    let db = match databases.get_opt(&db_name).await? {
+        Some(db) => db,
+        None => {
+            info!("OpenDcsDatabase {} does not yet exist", db_name);
+            return Ok(Action::requeue(Duration::from_secs(3600/4)));
+        },
+    };
+    let dbStatus = match db.status {
+        Some(s) => s.state.unwrap_or(MigrationState::Fresh),
+        None => return Ok(Action::requeue(Duration::from_secs(3600/4)));,
+    };
+
+    // find existing app
+    // if db ready make sure active
+    // if db preparinging to migrate bring counts to 0
+    // if db migrating, make sure nothing else starts
+    
+    Ok(Action::requeue(Duration::from_secs(3600 / 2)))
+}
+
+fn error_policy(
+    object: Arc<OpenDcsApp>,
+    err: &kube::Error,
+    ctx: Arc<Context<OpenDcsApp>>,
+) -> Action {
+    warn!("reconcile failed: {:?}", err);
+    let e = anyhow!("Api error {:?}", err);
+    ctx.metrics.reconcile.set_failure(&object, &e);
+    Action::requeue(Duration::from_secs(5 * 60))
+}
