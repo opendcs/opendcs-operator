0b7126a4f36af4d8db9ba006280f2b80c4d90e41
M. Allan Neilson <m.allan.neilson@gmail.com>
2026-05-03 19:37:00 -0700

    Apps controller test setup.

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,7 +3,14 @@ mod common;
 #[cfg(test)]
 mod tests {
 
-    use opendcs_controllers::api::v1::tsdb::database::MigrationState;
+    use kube::{
+        Api,
+        api::{ObjectMeta, Patch, PatchParams},
+    };
+    use opendcs_controllers::api::v1::tsdb::{
+        app::{OpenDcsApp, OpenDcsAppSpec},
+        database::MigrationState,
+    };
     use rstest::rstest;
     use tracing::info;
 
@@ -37,6 +44,45 @@ mod tests {
         let status = odcs_db.opendcs_database.status.clone().expect("No status?");
         assert!(status.state == Some(MigrationState::Ready));
         assert!(status.applied_schema_version == Some(upgrade_image.into()));
+
+        assert!(odcs_db.delete().await);
+
+        info!("OpenDCS Database Removed.");
+        db.close().await.expect("Unable to cleanup resources.");
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_app_starts(#[future] k8s_inst: &K8s) {
+        let k8s_inst = k8s_inst.await;
+        let client = k8s_inst.get_client();
+        let base_image = "ghcr.io/opendcs/migration:main-nightly";
+
+        let db = k8s_inst.create_database("app").await;
+
+        let odcs_db = OpenDcsTestDatabase::new(client.clone(), "testdb-app", &db, base_image).await;
+        let status = odcs_db.opendcs_database.status.clone().expect("No status?");
+        assert!(status.state == Some(MigrationState::Ready));
+        assert!(status.applied_schema_version == Some(base_image.into()));
+
+        let app = OpenDcsApp {
+            metadata: ObjectMeta {
+                name: Some("testapp-web-api".into()),
+                ..odcs_db.opendcs_database.metadata.clone()
+            },
+            spec: OpenDcsAppSpec {
+                application: "web-api".into(),
+                version: Some("main-nightly".into()),
+                ..Default::default()
+            },
+            status: None,
+        };
+        let pp = PatchParams::apply("testapp-patch");
+        let dcs_app_api: Api<OpenDcsApp> = Api::default_namespaced(client.clone());
+        dcs_app_api
+            .patch("testapp-patch", &pp, &Patch::Apply(app.clone()))
+            .await
+            .expect("Unable to create OpenDCS App Instance.");
 
         assert!(odcs_db.delete().await);
 
